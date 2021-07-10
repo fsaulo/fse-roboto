56c26a145e8f15953dfd4827565fd479fa8fe875
Saulo G. Felix <meetnapoleon@protonmail.ch>
2021-07-10 19:30:12 -0300

    Enable external interrupt on PD5
    The modulated signal comming from the RX generates an interrupt
    request on border rising and falling edge clk cycle. A timer [TCCR2] is set
    to count every cycle of the external clock divided by a 256-prescaler.
    
    When a change of state (from LOW to a HIGH) is detected, the value
    of the timer2 register [TCNT2] is fetched filtered and connected to the OCR1A
    register (leftmost count value) to determine the duty cycle; that way
    the motor is controlled by the PB1 PWM output signal.

--- src/fseroboto.c
+++ src/fseroboto.c
@@ -14,17 +14,28 @@ int main() {
 #define RXPIN  ((1 << PD5) & PIND) >> PIND5
 
 enum { LOW, HIGH };
+
 void
 ioinit () {
     DDRD = _BV(PD3);
     DDRB = _BV(PB1);
+
+    /* Enable Timer1 as fast PWM controlled by ICR1 */
+    TCCR1A = _BV(COM1A1) | _BV(COM1A0) | _BV(WGM11);
+    TCCR1B = _BV(WGM13) | _BV(WGM12) | _BV(CS12); 
+    ICR1 = 255;
+    OCR1A = 0;
+
     TCCR2B = _BV(CS22);
     serial_init();
 
+    /* Redirect stdin/stdiout to serial port */
     stdin  = &SERIALIN;
     stdout = &SERIALOUT;
 
-    printf("HELLO USART!\n");
+    /* Enable global interrupt flag */
+    sei();
+}
 
 int 
 main () {
@@ -40,5 +51,6 @@ ISR (PCINT2_vect) {
 
     if (state == LOW) {
         RFPWM = (TCNT2) <= 255 ? 255 - TCNT2: 0;
+        OCR1A = RFPWM;
     } else TCNT2 = 0;
 }
