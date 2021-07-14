4ef38d67f3fb0dc0d1575a5a4d32c6bb3059a68f
Saulo G. Felix <meetnapoleon@protonmail.ch>
2021-07-14 00:53:26 -0300

    Simplifying a lot of stuff, comming back to external interrupts
    This patch simplifies a lot of the parameters introduced by b467cff
    Most of these parameters did not survive a reset. The parameters is some
    sort of "fine tune", so the fine tuning should be performed every reset,
    this is tottaly impractical and I just figured out after spending a few hours
    tweaking it.
    
    So turns ou that syncing by external interrupts isn't that bad if done
    right, and if the frequency of external clock isn't too great;

--- src/fseroboto.c
+++ src/fseroboto.c
@@ -12,51 +12,41 @@
 #define TOPCT2  (0x00FF)
 #define TOPPOSC (0x029A)
 
-#define RXPWM   (PD5)
 #define PWM1OUT (PB1)
 #define PWM2OUT (PB2)
 
-#define RX1PIN  ((1 << PD6) & PIND) >> PIND6 == 0 ? LOW : HIGH
-#define RX2PIN  ((1 << PD3) & PIND) >> PIND3 == 0 ? LOW : HIGH
-#define RX3PIN  ((1 << PD5) & PIND) >> PIND5 == 0 ? LOW : HIGH
-#define RX4PIN  ((1 << PB3) & PIND) >> PINB3 == 0 ? LOW : HIGH
-
+#define RX2PIN  ((1 << PD2) & PIND) >> PIND2 == 0 ? LOW : HIGH
+#define RX3PIN  ((1 << PD3) & PIND) >> PIND3 == 0 ? LOW : HIGH
 
 enum { LOW, HIGH };
 
 typedef struct {
-    uint8_t CH1;
     uint8_t CH2;
     uint8_t CH3;
-    uint8_t CH4;
-} receiver;
+} sync;
 
-volatile receiver RXSTR;
+volatile sync RXSYNC;
 
 volatile uint32_t POSCNT;
 volatile uint32_t RF3PWM;
-volatile uint32_t RF1CNT;
 volatile uint32_t RF2CNT;
 volatile uint32_t RF3CNT;
 
 void
 ioinit () {
     DDRB = _BV (PWM1OUT) | _BV (PWM2OUT);
+    DDRD = _BV (PD6);
+    DDRB = _BV (PB1);
 
-    TCCR0A = _BV (WGM01);
-    TCCR0B = _BV (CS00);
+    /* External interrupt */
+    EICRA = _BV (ISC10) | _BV (ISC00);
+    EIMSK = _BV (INT1) | _BV (INT0);
 
-    OCR0A = 1;
-    TIMSK0 = _BV (OCIE0A);
-
-    /* Enable Timer1 as fast PWM controlled by ICR1 */
-    TCCR1A = _BV (COM1A1) | _BV (COM1A0) | _BV (COM1B1) | _BV (COM1B0) | _BV (WGM11);
-    TCCR1B = _BV (WGM13) | _BV (WGM12) | _BV (CS10); 
-
-    ICR1 = TOPCT2;
-    OCR1A = OCR1B = 0;
-
-    TCCR2B = _BV (CS22);
+    /* Timer 2 */
+    TCCR2A = _BV (COM2A0) | _BV (WGM21);
+    TCCR2B = _BV (CS21);
+    TIMSK2 = _BV (OCIE2A);
+    OCR2A = 1;
 
     serial_init();
 
@@ -64,13 +54,9 @@ ioinit () {
     stdout = &SERIALOUT;
 
     POSCNT = 0;
-    RF1CNT = 0;
-    RF2CNT = 0;
     RF3PWM = 0;
-
-    RXSTR.CH3 = HIGH;
-    RXSTR.CH2 = HIGH;
-    RXSTR.CH1 = HIGH;
+    RF3CNT = 0;
+    RF2CNT = 0;
 
     sei();
 }
@@ -97,85 +83,22 @@ tune_in_ch1() {
     }
 }
 
-void
-tune_in_ch2() {
-    if (RX2PIN == LOW && RXSTR.CH2 == LOW) {
-        _NOP();
-    } else if ((RX2PIN == HIGH && RXSTR.CH2 == LOW) || RX2PIN == RXSTR.CH2) {
-        RF2CNT += 1;
-        RXSTR.CH2 = RX2PIN;
-    } else if (RX2PIN != RXSTR.CH2) {
-        RXSTR.CH2 = RX2PIN;
-    }
+ISR (TIMER2_COMPA_vect) {
+    if (RX3PIN == HIGH && RXSYNC.CH3 == HIGH)
+        RF3CNT++;
+    else RXSYNC.CH3 = LOW;
+
+    if (RX2PIN == HIGH && RXSYNC.CH2 == HIGH)
+        RF2CNT++;
+    else RXSYNC.CH2 = LOW;
 }
 
-void
-tune_in_ch3() {
-    if (RX3PIN == LOW && RXSTR.CH3 == LOW) {
-        _NOP();
-    } else if ((RX3PIN == HIGH && RXSTR.CH3 == LOW) || RX3PIN == RXSTR.CH3) {
-        RF3CNT += 2;
-        RXSTR.CH3 = RX3PIN;
-    } else if (RX3PIN != RXSTR.CH3) {
-        RXSTR.CH3 = RX3PIN;
-    }
+ISR (INT0_vect) {
+    RXSYNC.CH2 = HIGH;
+    RF2CNT = 0;
 }
 
-ISR (TIMER0_COMPA_vect) {
-    volatile uint8_t index = 0;
-    volatile uint16_t sigch1aux_vect[FILTER_LENGTH];
-    volatile uint16_t sigch2aux_vect[FILTER_LENGTH];
-    volatile uint16_t sigch3aux_vect[FILTER_LENGTH];
-
-    tune_in_ch1();
-    tune_in_ch2();
-    tune_in_ch3();
-    
-    if (POSCNT == TOPPOSC) {
-        /* Here we perform a frequency division followed by a simple moving */
-        /* average filtering */
-        if (index < FILTER_LENGTH) {
-            sigch3aux_vect[index] = RF3CNT;
-            sigch2aux_vect[index] = RF2CNT;
-            sigch1aux_vect[index] = RF1CNT;
-            index++;
-        } else {
-            index = 0;
-        }
-        
-        /* After filtering the variables are used to set the PWM duty cycle */
-        /* This determines the output speed of the DC motors */
-        for (index = 0; index < FILTER_LENGTH; index++) {
-            RF1CNT += 0.9 * sigch1aux_vect[index];
-            RF2CNT += 1.3 * sigch2aux_vect[index];
-            RF3CNT += 2.4 * sigch3aux_vect[index];
-        }
-
-        RF1CNT = (210 - (RF1CNT / (FILTER_LENGTH+1))) & 0xFF; 
-        RF2CNT = ((RF2CNT / (FILTER_LENGTH+1))) & 0x1F4;      
-        RF3CNT = ~(140 - (RF3CNT / (FILTER_LENGTH+1))) & 0xFF;
-
-        if (RF2CNT > 45 && RF2CNT < 60) {
-            TCCR1A &= 0b11;
-            PORTB &= ~(1 << PB1);
-            PORTB &= ~(1 << PB2);
-            OCR1A = OCR1B = 0;
-        } else if (RF2CNT > 70) {
-            TCCR1A &= 0b11;
-            TCCR1A = _BV (COM1A0) | _BV (COM1A1);
-            OCR1A = 0;
-        } else if (RF2CNT < 30) {
-            TCCR1A &= 0b11;
-            TCCR1A = _BV (COM1B0) | _BV (COM1B1);
-            OCR1A = 0;
-        }
-
-        OCR1A = RF3CNT;
-        OCR1B = RF3CNT;
-
-
-        /* End of clock cycle. Clear timing variable */
-        POSCNT = RF3CNT = RF2CNT = 0;
-    }
-    POSCNT += 1;
+ISR (INT1_vect) {
+    RXSYNC.CH3 = HIGH;
+    RF3CNT = 0;
 }
